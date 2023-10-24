7ff5e32ca03cf1dfb88fd133675668b8dc5bed29
pal2022 <132899382+pal2022@users.noreply.github.com>
2023-10-24 09:23:44 -0700

    milestone 2 main

--- src/run/Main.java
+++ src/run/Main.java
@@ -1,11 +1,16 @@
 package run;
 
+
+import game.ComputerControlledGame;
+import game.GameController;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import view.WorldImage;
 import world.Mansion;
+import world.Player;
 import world.Space;
 import world.WorldBuilder;
-import world.WorldImage;
+//import world.MockMansion;
 
 /**
  * This is the main class for running the application.
@@ -19,80 +24,25 @@ public class Main {
    * @param args pass parameters from command-in-line argument.
    */
   public static void main(String[] args) {
-    String filePath = Main.class.getResource("PalMansion.txt").getPath();
-    //    String filePath2 = Main.class.getResource("Pal.txt").getPath();    
-    Mansion world = WorldBuilder.build(filePath);
-    //    Mansion world2 = WorldBuider.build(filePath2);
-    WorldImage display = new WorldImage(world); 
 
-    Scanner scanner = new Scanner(System.in);
+    //C:\Users\hp\eclipse-workspace\lab00_getting_started\MileStone1\model\src\run\PalMansion.txt
 
-    while (true) {
-      displayMenu();
-      try {
-        int choice = scanner.nextInt();
-        scanner.nextLine();
-
-        if (choice == 1) {
-          System.out.print("Player has moved from : " + world.getPlayerRoomName());
-          world.targetCharacter.movePlayer();
-          System.out.print(" to : " + world.getPlayerRoomName());
-        } else if (choice == 2) {
-          System.out.println("The world image display");
-          display.showImage();
-        } else if (choice == 3) {
-          System.out.print("Enter room index\n");
-          try {
-            int id = scanner.nextInt();
-            scanner.nextLine();
-            if (id < 0 || id > world.getRoomCount()) {
-              throw new IllegalArgumentException("Invalid room index.");
-            }
-            Space room = world.rooms.get(id);
-            room.displayInformation();
-          } catch (IllegalArgumentException e) {
-            System.out.println("Invalid input.");
-            scanner.nextLine();
-          }
-          
-          
-        } else if (choice == 4) {
-          System.out.print("Enter room index\n");
-          try {
-            int id = scanner.nextInt();
-            scanner.nextLine();
-            if (id < 0 || id > world.getRoomCount()) {
-              throw new IllegalArgumentException("Invalid room index.");
-            }
-            Space room = world.rooms.get(id);
-            room.displayNeighbours();
-          } catch (IllegalArgumentException e) {
-            System.out.println("Invalid input.");
-            scanner.nextLine();
-          }
-        } else if (choice == 5) {
-          break;
-        } else {
-          System.out.println("Invalid input.");
-        }
-      } catch (InputMismatchException e) {
-        System.out.println("Invalid input.");
-        scanner.nextLine();
-      }
+    System.out.println("Enter file path and max number of turns");
+    if (args.length < 2) {
+      System.out.println("Enter file path and max number of turns");
     }
-  }
 
-  /**
-   * This method has the possible options the user can choose from.
-   */
-  public static void displayMenu() {
-    System.out.println("\n\nMenu\n\n");
-    System.out.println("1. Move Character");
-    System.out.println("2. Image Output");
-    System.out.println("3. Display room");
-    System.out.println("4. Display neighbours of room");
-    System.out.println("5. Exit menu");
-    
-  }
+    String filePath = args[0];
+    int maxTurns = Integer.parseInt(args[1]);
 
+    Mansion world = WorldBuilder.build(filePath);
+  
+    GameController game = new GameController(world, maxTurns, filePath);
+
+    game.startGame();
+    //    ComputerControlledGame cGame = new ComputerControlledGame();
+  
+   
+    //       cGame.startGame();
+  }
 }
